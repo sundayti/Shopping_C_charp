783e7d00b6af2051fc6fa047766f507c9bd22d43
sundayti <pankratovvlad1@gmail.com>
2025-06-17 22:34:12 +0300

    Add Kafka

--- PaymentsServer/PaymentsServer.Presentation/Program.cs
+++ PaymentsServer/PaymentsServer.Presentation/Program.cs
@@ -1,20 +1,19 @@
 using System.Reflection;
-using MediatR;
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using PaymentsServer.Application;
 using PaymentsServer.Domain.Interfaces;
 using PaymentsServer.Infrastructure.Persistence;
 using PaymentsServer.Infrastructure.Repositories;
+using PaymentsServer.Infrastructure.Workers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
 
 var connectionString = builder.Configuration.GetConnectionString("Postgres");
 if (string.IsNullOrWhiteSpace(connectionString))
-{
     throw new InvalidOperationException("Connection string 'Postgres' is not configured.");
-}
 
 builder.Services.AddDbContext<PaymentAccountsDbContext>(options =>
     options.UseNpgsql(connectionString)
@@ -25,28 +24,45 @@ builder.Services.AddScoped<IInboxRepository, InboxRepository>();
 builder.Services.AddScoped<IOutboxRepository, OutboxRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-var applicationAssembly = typeof(ApplicationAssemblyReference).GetTypeInfo().Assembly;
 
+var applicationAssembly = typeof(ApplicationAssemblyReference).GetTypeInfo().Assembly;
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssemblies(applicationAssembly)
 );
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(5002, listenOptions =>
-    {
-        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-    });
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2);
 });
+
+var kafkaSection = builder.Configuration.GetSection("Kafka");
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = kafkaSection.GetValue<string>("BootstrapServers") ?? throw new InvalidOperationException("Kafka:BootstrapServers is not configured."),
+    GroupId            = kafkaSection.GetValue<string>("GroupId") ?? "payments-group",
+    AutoOffsetReset    = AutoOffsetReset.Earliest,
+    EnableAutoCommit   = false
+};
+builder.Services.AddSingleton(consumerConfig);
+
+builder.Services.AddSingleton<IConsumer<string, string>>(sp =>
+    new ConsumerBuilder<string, string>(sp.GetRequiredService<ConsumerConfig>())
+        .SetKeyDeserializer(Deserializers.Utf8)
+        .SetValueDeserializer(Deserializers.Utf8)
+        .Build());
+
+builder.Services.AddHostedService<StupidInboxWorker>();
 
 var app = builder.Build();
 
-using var scope = app.Services.CreateScope();
-var dbContext = scope.ServiceProvider.GetRequiredService<PaymentAccountsDbContext>();
-dbContext.Database.Migrate();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PaymentAccountsDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapControllers();
 
